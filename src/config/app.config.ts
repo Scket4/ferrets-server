8b28ce6547900d478b2e9aa6757343a09c7880ee
alxxsck <alexeygolubev921@gmail.com>
2022-03-24 21:04:57 +0300

    fixes

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,6 +1,6 @@
 export default () => ({
   environment: process.env.NODE_ENV || 'development',
-  port: parseInt(process.env.PORT, 10) || 4000,
+  port: parseInt(process.env.PORT, 10),
   secret_key: process.env.SECRET_KEY,
   base_url: process.env.BASE_URL,
 });
